3e264a7bb85df72e4ed34426b25856d54e563fd4
Nikita Elfimov <ne@atls.team>
2022-04-08 18:05:02 +0500

    feat: init gateway

--- com-port/com-port/index.js
+++ com-port/com-port/index.js
@@ -1,7 +1,35 @@
-/* eslint-disable */
+'use strict'
 
-import SerialPort from 'serialport'
-import DelimiterParser from '@serialport/parser-delimiter'
+const { SerialPort } = require('serialport')
+const { DelimiterParser } = require('@serialport/parser-delimiter')
+const fastify = require('fastify')
+
+const server = fastify({ logger: true })
+
+let ready = false
+let data
+
+const applyResult = (result) => {
+	data = result
+	ready = true
+}
+
+const reset = () => {
+	data = undefined
+	ready = false
+}
+
+server.get('/weight', async (req, res) => {
+	requestWeight()
+
+	while(!ready) {
+		// do nothing
+	}
+
+	reset()
+
+	return { weight: data }
+})
 
 const port = new SerialPort({
 	path: 'COM1',
@@ -18,9 +46,9 @@ const CMD_BARCODE = "S08";
 const EOC = 0x0D;
 const CMD_GETWEIGHT = "S11";
 
-const parser = port.pipe(new DelimiterParser({ 
-	delimiter: [EOC], 
-	includeDelimiter: false 
+const parser = port.pipe(new DelimiterParser({
+	delimiter: [EOC],
+	includeDelimiter: false
 }));
 
 port.open((e) => {
@@ -31,24 +59,22 @@ port.open((e) => {
 	console.log(`# Port opened OK`);
 });
 
-port.on('open', () => {
-	requestWeight();
-});
-
 port.on('error', (e) => {
 	console.log(`% Port error: ${e}`);
 });
 
 parser.on('data', (data) => {
-  console.log(`# Port data: ${data}`);
+	console.log(`# Port data: ${data}`);
 
-  let cmd = data.toString('ascii', 0, 3);
-  let payload = Buffer.alloc(data.length - 3);
-  data.copy(payload, 0, 3, data.length);
+	let cmd = data.toString('ascii', 0, 3);
+	let payload = Buffer.alloc(data.length - 3);
+	data.copy(payload, 0, 3, data.length);
 
-  console.log(`# Got cmd: ${cmd}`);
+	console.log(`# Got cmd: ${cmd}`);
 
-  parseCmd(cmd, payload);
+	const { weight } = parseCmd(cmd, payload);
+
+	applyResult(weight)
 });
 
 function parseCmd(cmd, payload) {
@@ -58,13 +84,15 @@ function parseCmd(cmd, payload) {
 
 			console.log(`# Weight value ${weight}`);
 
-			break;
+			return { weight }
 		case CMD_BARCODE:
 			let barcode = payload.toString('ascii', 1);
-			
+
 			console.log(`# Got barcode ${barcode}`);
 
-			break;
+			return { barcode }
+		default:
+			return { weight: undefined, barcode: undefined }
 	}
 }
 
@@ -75,6 +103,16 @@ function requestWeight() {
 		port.write(CMD_GETWEIGHT);
 		port.write(Buffer.from([EOC, EOC]));
 	}
-
-	setTimeout(requestWeight, 1000);
 }
+
+const start = async () => {
+	try {
+		await fastify.listen(3000)
+	} catch (err) {
+		console.log(err)
+		fastify.log.error(err)
+		process.exit(1)
+	}
+}
+
+start()
