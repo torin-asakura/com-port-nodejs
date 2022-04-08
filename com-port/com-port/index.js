32842e2ad62c6d7afcde88e3823100431b19689e
Nikita Elfimov <ne@atls.team>
2022-04-08 19:55:58 +0500

    chore: eslint disable

--- com-port/com-port/index.js
+++ com-port/com-port/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 'use strict'
 
 const { SerialPort } = require('serialport')
@@ -9,97 +11,99 @@ const server = fastify({ logger: true })
 const observers = []
 
 server.get('/weight', async (req, res) => {
-	requestWeight()
+  requestWeight()
 
-	return new Promise(resolve => {
-		observers.push((value) =>  {
-			observers.pop()
-			resolve(value)
-		})
-	})
+  return new Promise((resolve) => {
+    observers.push((value) => {
+      observers.pop()
+      resolve(value)
+    })
+  })
 })
 
 const port = new SerialPort({
-	path: 'COM1',
-	baudRate: 9600,
-	parity: 'none',
-	dataBits: 8,
-	stopBits: 1,
-	rtscts: true,
-	autoOpen: false
-});
+  path: 'COM1',
+  baudRate: 9600,
+  parity: 'none',
+  dataBits: 8,
+  stopBits: 1,
+  rtscts: true,
+  autoOpen: false,
+})
 
-const CMD_WEIGHT = "S11";
-const CMD_BARCODE = "S08";
-const EOC = 0x0D;
-const CMD_GETWEIGHT = "S11";
+const CMD_WEIGHT = 'S11'
+const CMD_BARCODE = 'S08'
+const EOC = 0x0d
+const CMD_GETWEIGHT = 'S11'
 
-const parser = port.pipe(new DelimiterParser({
-	delimiter: [EOC],
-	includeDelimiter: false
-}));
+const parser = port.pipe(
+  new DelimiterParser({
+    delimiter: [EOC],
+    includeDelimiter: false,
+  })
+)
 
 port.open((e) => {
-	if (e) {
-		return console.log(`% Port open error ${e}`);
-	}
+  if (e) {
+    return console.log(`% Port open error ${e}`)
+  }
 
-	console.log(`# Port opened OK`);
-});
+  console.log(`# Port opened OK`)
+})
 
 port.on('error', (e) => {
-	console.log(`% Port error: ${e}`);
-});
+  console.log(`% Port error: ${e}`)
+})
 
 parser.on('data', (data) => {
-	console.log(`# Port data: ${data}`);
+  console.log(`# Port data: ${data}`)
 
-	let cmd = data.toString('ascii', 0, 3);
-	let payload = Buffer.alloc(data.length - 3);
-	data.copy(payload, 0, 3, data.length);
+  let cmd = data.toString('ascii', 0, 3)
+  let payload = Buffer.alloc(data.length - 3)
+  data.copy(payload, 0, 3, data.length)
 
-	console.log(`# Got cmd: ${cmd}`);
+  console.log(`# Got cmd: ${cmd}`)
 
-	const { weight } = parseCmd(cmd, payload);
+  const { weight } = parseCmd(cmd, payload)
 
-	observers.forEach(observer => observer(JSON.stringify({ weight })))
-});
+  observers.forEach((observer) => observer(JSON.stringify({ weight })))
+})
 
 function parseCmd(cmd, payload) {
-	switch (cmd) {
-		case CMD_WEIGHT:
-			let weight = parseInt(payload.toString('ascii'));
+  switch (cmd) {
+    case CMD_WEIGHT:
+      let weight = parseInt(payload.toString('ascii'))
 
-			console.log(`# Weight value ${weight}`);
+      console.log(`# Weight value ${weight}`)
 
-			return { weight }
-		case CMD_BARCODE:
-			let barcode = payload.toString('ascii', 1);
+      return { weight }
+    case CMD_BARCODE:
+      let barcode = payload.toString('ascii', 1)
 
-			console.log(`# Got barcode ${barcode}`);
+      console.log(`# Got barcode ${barcode}`)
 
-			return { barcode }
-		default:
-			return { weight: undefined, barcode: undefined }
-	}
+      return { barcode }
+    default:
+      return { weight: undefined, barcode: undefined }
+  }
 }
 
 function requestWeight() {
-	if (port.isOpen) {
-		console.log(`# Requesting weight`);
+  if (port.isOpen) {
+    console.log(`# Requesting weight`)
 
-		port.write(CMD_GETWEIGHT);
-		port.write(Buffer.from([EOC, EOC]));
-	}
+    port.write(CMD_GETWEIGHT)
+    port.write(Buffer.from([EOC, EOC]))
+  }
 }
 
 const start = async () => {
-	try {
-		await server.listen(3000)
-	} catch (err) {
-		console.log(err)
-		process.exit(1)
-	}
+  try {
+    await server.listen(3000)
+  } catch (err) {
+    console.log(err)
+    process.exit(1)
+  }
 }
 
 start()
