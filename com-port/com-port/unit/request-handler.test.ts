dddd6177aed051a6c2cbda82e9b5f316e032b00c
Nikita Elfimov <ne@atls.team>
2022-04-10 01:25:43 +0500

    coverage: update tests

--- com-port/com-port/unit/request-handler.test.ts
+++ com-port/com-port/unit/request-handler.test.ts
@@ -2,11 +2,11 @@ const { requestHandler } = require('../request-handler')
 
 describe('suit for com-port-nodejs', () => {
   it('should handle request correctly', async () => {
-    const observers = []
+    const observer = { current: undefined }
     const requestedData = 'Requested data'
 
     const onData = (data) => {
-      observers.forEach(observer => observer(data))
+      observer.current(data)
     }
 
     const requestSomething = () => {
@@ -15,19 +15,20 @@ describe('suit for com-port-nodejs', () => {
       }, 1000)
     }
 
-    const data = await requestHandler(requestSomething, observers)
+    const request = requestHandler(requestSomething, observer)
+    const result = await request()
 
-    expect(data).toBe(requestedData)
+    expect(result).toBe(requestedData)
   })
 
-  it('should handle multiple requests correctly', async () => {
-    const observers = []
+  it('should handle multiple requests sync', async () => {
+    const observer = { current: undefined }
     const requestedData1 = 'Requested data 1'
     const requestedData2 = 'Requested data 2'
     const requestedData3 = 'Requested data 3'
 
     const onData = (data) => {
-      observers.forEach(observer => observer(data))
+      observer.current(data)
     }
 
     function* requestGenerator() {
@@ -44,12 +45,33 @@ describe('suit for com-port-nodejs', () => {
 
     const requestSomething = requestGenerator()
 
-    const data1 = await requestHandler(requestSomething.next().value, observers)
-    const data2 = await requestHandler(requestSomething.next().value, observers)
-    const data3 = await requestHandler(requestSomething.next().value, observers)
+    const request1 = requestHandler(requestSomething.next().value, observer)
+    const request2 = requestHandler(requestSomething.next().value, observer)
+    const request3 = requestHandler(requestSomething.next().value, observer)
 
-    expect(data1).toBe(requestedData1)
-    expect(data2).toBe(requestedData2)
-    expect(data3).toBe(requestedData3)
+    const result1 = await request1()
+    const result2 = await request2()
+    const result3 = await request3()
+
+    expect(result1).toBe(requestedData1)
+    expect(result2).toBe(requestedData2)
+    expect(result3).toBe(requestedData3)
+  })
+
+  it('should handle multiple requests at a time', async () => {
+    const observer = { current: undefined }
+    const requestedData = 'Requested data'
+
+    const onData = (data) => {
+      observer.current(data)
+    }
+
+    requestHandler(() => onData('Should fail'), observer)()
+    requestHandler(() => onData('Should fail'), observer)()
+
+    const request = requestHandler(() => onData(requestedData), observer)
+    const result = await request()
+
+    expect(result).toBe(requestedData)
   })
 })
