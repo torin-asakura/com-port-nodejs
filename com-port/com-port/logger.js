bb7463ce75865d3a4aca4ed94460a675ec118871
Nikita Elfimov <ne@atls.team>
2022-04-11 00:28:37 +0500

    feat: write logs to file

---
+++ com-port/com-port/logger.js
@@ -0,0 +1,16 @@
+const createLogger = () => {
+  const { createWriteStream } = require('fs')
+
+  const date = Date.now()
+  const time = `${date.getFullYear()}-${date.getMonth()}-${date.getHours}h-${date.getMinutes()}m-${date.getSeconds}s`
+
+  const stream = createWriteStream()
+
+  const write = (payload) => {
+    stream.write(`${time}: ${payload}`)
+  }
+
+  return { write }
+}
+
+module.exports = { createLogger }
