eb53bf20770f2cf9e17f47dded1d4cc05878d1af
Nikita Elfimov <ne@atls.team>
2022-04-12 02:06:13 +0500

    feat: support parallel requests for a type

--- com-port/com-port/logger.js
+++ com-port/com-port/logger.js
@@ -1,8 +1,12 @@
+/* eslint-disable no-console */
+
 const createLogger = () => {
   const { createWriteStream } = require('fs')
 
   const date = Date.now()
-  const time = `${date.getFullYear()}-${date.getMonth()}-${date.getHours}h-${date.getMinutes()}m-${date.getSeconds}s`
+  const time = `${date.getFullYear()}-${date.getMonth()}-${date.getHours}h-${date.getMinutes()}m-${
+    date.getSeconds
+  }s`
 
   const stream = createWriteStream()
 
--- com-port/com-port/register-service.js
+++ com-port/com-port/register-service.js
@@ -3,7 +3,7 @@ const { Service } = require('node-windows')
 const svc = new Service({
   name: 'Com-port gateway',
   description: 'Com-port gateway',
-  script: `C:\\User\\37525\\Desktop\\com-port-nodejs\\com-port\\com-port\\index.js`
+  script: `C:\\User\\37525\\Desktop\\com-port-nodejs\\com-port\\com-port\\index.js`,
 })
 
 svc.on('install', () => {
